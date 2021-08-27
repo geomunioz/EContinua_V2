9d9cd0d4a5d6f08870e4750c28f609267c02923e
jorgetmz@outlook.com <jorgetmz@outlook.com>
2021-08-26 21:11:45 -0500

    Estilos responsive en login

--- js/eventos.js
+++ js/eventos.js
@@ -1,17 +1,40 @@
+/*Contraseña */
+const btn_password = document.querySelector('.icon.p.one');
+
 /*Apertura y cierre de menu mobile */
 const btn_menuHeader = document.querySelector('#btn_menuHeader');
 const cancel = document.querySelector('#menu-c');
 
-function menuEnable(){
-    let menuMobile = document.querySelector('.menu--mobile.disable');
-    menuMobile.className = "menu--mobile.enable";
+function menuMobile(){
+    let menuMobile = document.querySelector('.menu-mobile');  
+    if(menuMobile.className === "menu-mobile disable") {
+        menuMobile.className = "menu-mobile enable";
+    }else{
+        menuMobile.className = "menu-mobile disable";
+    }
 }
 
-function menuDisable(){
-    let menuMobile = document.querySelector('.menu--mobile.enable');
-    menuMobile.className = "menu--mobile disable";
+function visible(){
+    let password = document.querySelector('#password');
+    if(btn_password.className === "icon p one"){
+        let node = document.querySelector('.far.fa-eye');
+
+        node.className = "fas fa-eye-slash";
+        password.type = "text"
+        btn_password.className = "icon p two"
+    }else{
+        let node = document.querySelector('.fas.fa-eye-slash');
+        node.className = "far fa-eye";
+        password.type = "password"
+        btn_password.className = "icon p one"
+    }
+}
+
+/*Visibilidad de contraseña */
+if(btn_password!=null){
+    btn_password.addEventListener('click', visible);
 }
 
 /*Control menu Mobile */
-btn_menuHeader.addEventListener('click', menuEnable);
-cancel.addEventListener('click', menuDisable);
+btn_menuHeader.addEventListener('click', menuMobile);
+cancel.addEventListener('click', menuMobile);
